923854c667119c3ef454e52ada91e8a704ffb6a1
Parker <parkied@gmail.com>
2024-05-26 13:52:11 -0700

    Upgrade deps, json feed

---
+++ src/pages/feed/feed.json.js
@@ -0,0 +1,38 @@
+import getCombinedPosts from '@utils/getCombinedPosts';
+import { metadata } from 'src/metadata';
+
+function formatPostMetadataForJsonFeed(post) {
+    const url = post.bms
+        ? post.data.slug
+        : `${import.meta.env.SITE}/blog/${post.slug}`;
+    const formattedObject = {
+        id: url,
+        url: url,
+        title: post.data.title,
+        content_text: post.data.description,
+        date_published: new Date(post.data.date),
+    };
+    return formattedObject;
+}
+
+export async function GET(context) {
+    const posts = await getCombinedPosts();
+    const formattedPosts = posts.map((post) =>
+        formatPostMetadataForJsonFeed(post)
+    );
+
+    return new Response(
+        JSON.stringify({
+            version: 'https://jsonfeed.org/version/1.1',
+            title: metadata.title,
+            home_page_url: import.meta.env.SITE,
+            feed_url: import.meta.env.SITE + '/feed/feed.json',
+            items: formattedPosts,
+        }),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+    );
+}
