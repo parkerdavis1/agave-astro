0d580d453d27df2c85abfac42a4c2c5a1634688d
Parker <parkied@gmail.com>
2024-06-14 09:17:23 -0700

    changes

--- src/pages/api/deleteComment.ts
+++ src/pages/api/deleteComment.ts
@@ -6,15 +6,16 @@ export const GET: APIRoute = async ({ request }) => {
     const params = new URLSearchParams(url.search);
     const commentId = params.get('commentId');
     const secret = params.get('secret');
-    if (!commentId) return;
-    if (!secret) return;
+    if (!commentId || !secret) return new Response('Error', { status: 500 });
 
     if (secret !== import.meta.env.DELETE_COMMENT_SECRET) {
-        return;
+        return new Response('Wrong secret', { status: 400 });
     }
 
     await db
         .update(Comments)
         .set({ deleted: true })
         .where(eq(Comments.id, commentId));
+
+    return new Response('Success', { status: 200 });
 };
