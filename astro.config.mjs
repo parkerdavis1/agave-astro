6a382d4849eaa8ab0043a5f08a540bf6a447b190
Parker <parkied@gmail.com>
2025-10-13 13:09:24 -0700

    Update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,21 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 import expressiveCode from 'astro-expressive-code';
 import db from '@astrojs/db';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://parkerdavis.dev',
-    integrations: [
-        sitemap(),
-        expressiveCode(),
-        mdx(),
-        tailwind(),
-        svelte(),
-        db(),
-    ],
+    integrations: [sitemap(), expressiveCode(), mdx(), svelte(), db()],
     adapter: netlify(),
     vite: {
         optimizeDeps: {
             exclude: ['astro:db'],
         },
+
+        plugins: [tailwindcss()],
     },
 });
