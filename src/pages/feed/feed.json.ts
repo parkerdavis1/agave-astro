ac8c21f9bfac08136069277687d1484e20132bfb
Parker <parkied@gmail.com>
2025-11-09 10:15:20 -0700

    get rid of attempted feed tracking, back to static

--- src/pages/feed/feed.json.ts
+++ src/pages/feed/feed.json.ts
@@ -1,4 +1,4 @@
-export const prerender = false;
+// export const prerender = false;
 
 import getCombinedPosts from "@utils/getCombinedPosts";
 import { metadata } from "src/metadata";
@@ -43,24 +43,24 @@ export async function GET(context: APIContext) {
         formatPostMetadataForJsonFeed(post),
     );
 
-    // forward headers
-    const userAgent = context.request.headers.get("user-agent") || "";
-    const xff = context.request.headers.get("x-forwarded-for") || "";
+    // // forward headers
+    // const userAgent = context.request.headers.get("user-agent") || "";
+    // const xff = context.request.headers.get("x-forwarded-for") || "";
 
-    // send to Plausible
-    fetch("https://plausible.parkerdavis.dev/api/event", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "User-Agent": userAgent,
-            "X-Forwarded-For": xff,
-        },
-        body: JSON.stringify({
-            domain: new URL(context.request.url).origin,
-            name: "pageview",
-            url: context.request.url,
-        }),
-    }).catch((e) => console.warn("plausible failed", e));
+    // // send to Plausible
+    // fetch("https://plausible.parkerdavis.dev/api/event", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "User-Agent": userAgent,
+    //         "X-Forwarded-For": xff,
+    //     },
+    //     body: JSON.stringify({
+    //         domain: new URL(context.request.url).origin,
+    //         name: "pageview",
+    //         url: context.request.url,
+    //     }),
+    // }).catch((e) => console.warn("plausible failed", e));
 
     return new Response(
         JSON.stringify({
--- src/pages/feed/feed.xml.ts
+++ src/pages/feed/feed.xml.ts
@@ -1,33 +1,31 @@
-export const prerender = false;
+import rss, { getRssString } from "@astrojs/rss";
+import { filterDrafts } from "@utils/filterDrafts";
+import getCombinedPosts from "@utils/getCombinedPosts";
+import { metadata } from "src/metadata.ts";
+import { setDateTime } from "@utils/setDateTime";
 import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
-    const feedRes = await fetch(`${context.url.origin}/feed/feed-static.xml`);
-    const feedTxt = await feedRes.text();
+    const posts = (await getCombinedPosts()).filter(filterDrafts);
+    const items = posts.map((post) => {
+        let link = post.bms
+            ? post.data.slug
+            : `${context.site}blog/${post.slug}`;
 
-    // forward headers
-    const userAgent = context.request.headers.get("user-agent") || "";
-    const xff = context.request.headers.get("x-forwarded-for") || "";
+        return {
+            title: post.data.title,
+            // set date time to noon in AZ time
+            pubDate: new Date(setDateTime(post.data.date)),
+            link,
+            description: post.data.description,
+        };
+    });
 
-    // send to Plausible
-    fetch("https://plausible.parkerdavis.dev/api/event", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "User-Agent": userAgent,
-            "X-Forwarded-For": xff,
-        },
-        body: JSON.stringify({
-            domain: new URL(context.request.url).origin,
-            name: "pageview",
-            url: context.request.url,
-        }),
-    }).catch((e) => console.warn("plausible failed", e));
-
-    return new Response(feedTxt, {
-        headers: {
-            "Content-Type": "application/xml",
-            "Cache-Control": "no-cache",
-        },
+    return rss({
+        title: metadata.title,
+        description: metadata.description,
+        site: context.site || context.url.origin,
+        items,
+        stylesheet: "/rss/styles.xsl",
     });
 }
