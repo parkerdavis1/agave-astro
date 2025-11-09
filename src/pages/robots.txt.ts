b5df66a23f5971d2b0fb5527ae0f775f290b9875
Parker <parkied@gmail.com>
2025-11-09 09:33:20 -0700

    Robots.txt - disallow by default

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,16 +1,25 @@
-import type { APIRoute } from 'astro';
+import type { APIRoute } from "astro";
 
 const robotsTxt = `
 User-agent: *
-Allow: /
+Disallow: /
 
-Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
+User-agent: Googlebot
+User-agent: Bingbot
+User-agent: Bravebot
+User-agent: DuckDuckBot
+User-agent: DuckDuckGo-Favicons-Bot
+User-agent: Google Favicon
+User-agent: kagibot
+Disallow:
+
+Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
 
 export const GET: APIRoute = () => {
     return new Response(robotsTxt, {
         headers: {
-            'Content-Type': 'text/plain; charset=utf-8',
+            "Content-Type": "text/plain; charset=utf-8",
         },
     });
 };
