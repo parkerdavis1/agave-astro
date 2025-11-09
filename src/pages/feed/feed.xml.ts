1e9a480865f82eec9b3ede39a4d7b093e077bbed
Parker <parkied@gmail.com>
2025-11-09 10:08:17 -0700

    static feed, redirected

--- src/pages/feed/feed.xml.ts
+++ src/pages/feed/feed.xml.ts
@@ -1,27 +1,9 @@
 export const prerender = false;
-
-import rss, { getRssString } from "@astrojs/rss";
-import { filterDrafts } from "@utils/filterDrafts";
-import getCombinedPosts from "@utils/getCombinedPosts";
-import { metadata } from "src/metadata.ts";
-import { setDateTime } from "@utils/setDateTime";
 import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
-    const posts = (await getCombinedPosts()).filter(filterDrafts);
-    const items = posts.map((post) => {
-        let link = post.bms
-            ? post.data.slug
-            : `${context.site}blog/${post.slug}`;
-
-        return {
-            title: post.data.title,
-            // set date time to noon in AZ time
-            pubDate: new Date(setDateTime(post.data.date)),
-            link,
-            description: post.data.description,
-        };
-    });
+    const feedRes = await fetch(`${context.url.origin}/feed/feed-static.xml`);
+    const feedTxt = await feedRes.text();
 
     // forward headers
     const userAgent = context.request.headers.get("user-agent") || "";
@@ -42,15 +24,7 @@ export async function GET(context: APIContext) {
         }),
     }).catch((e) => console.warn("plausible failed", e));
 
-    const rssText = await getRssString({
-        title: metadata.title,
-        description: metadata.description,
-        site: context.site || context.url.origin,
-        items,
-        stylesheet: "/rss/styles.xsl",
-    });
-
-    return new Response(rssText, {
+    return new Response(feedTxt, {
         headers: {
             "Content-Type": "application/xml",
             "Cache-Control": "no-cache",
